5a824c21e4f5b15dff1f3ee2bda2469b8fc37005
Resha228 <123998484+Resha228@users.noreply.github.com>
2023-10-22 00:20:30 +0300

    start

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+class NamePage  {
+  title;
+}
+
+class Component <T>{
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<NamePage> {
   pageInfo () {
     console.log(this.props.title);
   }
